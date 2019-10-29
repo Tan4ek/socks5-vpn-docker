9ec5bfb7a9e70115bd896451c76e0baf214330b1
Sergei Samokhvalov <kolobok205@gmail.com>
2019-10-29 22:07:11 +0300

    add config file for mtproxy

---
+++ config_mtproxy_server.py
@@ -0,0 +1,22 @@
+PORT = 443
+
+# name -> secret (32 hex chars)
+USERS = {
+    "tg":  "00000000000000000000000000000000",
+    "tg2": "0123456789abcdef0123456789abcdef"
+}
+
+# Makes the proxy harder to detect
+# Can be incompatible with very old clients
+SECURE_ONLY = True
+
+# Makes the proxy even more hard to detect
+# Compatible only with the recent clients
+TLS_ONLY = True
+
+# The domain for TLS, bad clients are proxied there
+# Use random existing domain, proxy checks it on start
+TLS_DOMAIN = "github.com"
+
+# Tag for advertising, obtainable from @MTProxybot
+# AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
